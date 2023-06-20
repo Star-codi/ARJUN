2ad8e1942206e9dcdc6e636be9d5ef2dc00d114f
Star-codi <arjunrajan8@gmail.com>
2023-06-20 14:51:17 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "ARJUN";
+const meta = {
+    title: "ARJUN ",
+    description: "I’m Arjun R jr front-end developer _ Full stack devloper,currently searching for job ;_;",
+};
+
+const introdata = {
+    title: "I’m Arjun R",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I love playing fps games ",
+    },
+    description: ". I'm a web developer with a knack for creating functional and delightful websites. Let's bring some laughter to the digital world together!.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A Bit about Myself",
+    aboutme: "Hi, I'm Arjun from Kerala, India. I have a passion for web development and a wicked sense of humor. I create websites that are both functional and funny. Let's join forces and bring laughter to the digital world!",
+};
+
+const worktimeline = [{
+        jobtitle: "Learning Front End Development",
+        where: "Self Study",
+        date: "To infinty and beyond",
+    },
+    {
+        jobtitle: "B.Tech in Computer Science Engineering ",
+        where: "Mahaguru Institute of Technology",
+        date: "2017 - 2021",
+    },
+    {
+        jobtitle: "Higher Secondary School(12th) ",
+        where: "Infant Jesus ISC School",
+        date: "2016 - 2017",
+    },
+   ];
+
+const skills = [{
+        name: "java programming",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "C",
+        value: 60,
+    },
+    {
+        name: "html css",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Prepare to captivate your audience with stunning user interfaces and seamless user experiences. I specialize in creating visually appealing designs that not only look great but also ensure your users have a delightful journey through your website or application.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Harness the power of WordPress with our expert design services. We'll customize your WordPress website to reflect your unique style and message. From theme selection and layout customization to plugin integration, we'll create a professional and polished online presence for your business.",
+    },
+    {
+        title: "Java Desktop Application",
+        description: " Need a powerful desktop application that's both functional and user-friendly? Look no further! I create Java-based applications that streamline your processes and make everyday tasks a breeze. Let's transform your ideas into reliable and efficient software solutions.",
+    },
+   
+    {
+        title: "Front end projects",
+        description: "Brace yourself for interactive and engaging digital experiences. Using HTML, CSS, and JavaScript, I develop captivating websites and web applications that leave a lasting impression. From responsive designs to seamless navigation, I'll bring your vision to life.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "arjunrajan8@gmail.com",
+    YOUR_FONE: "(+91) 96334-33321",
+    description: "Feel free to reach out to me via email or phone. I'm excited to discuss your project and explore how we can collaborate to create something amazing!. ",
+    // emailjs.com account 
+    YOUR_SERVICE_ID: "service_uq57q8l",
+    YOUR_TEMPLATE_ID: "template_q5y1q7h",
+    YOUR_USER_ID: "i3G3FoY2htvkuiLOW",
+};
+
+const socialprofils = {
+    github: "https://github.com/Star-codi",
+    facebook: "https://www.facebook.com/arjun.rajan.798",
+    linkedin: "https://www.linkedin.com/in/arjun-r-/",
+   // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
